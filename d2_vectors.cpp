417ae574e568a44bbce335eb9114438e5ed309b8
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-01-18 22:56:00 +0530

    ...

--- d2_vectors.cpp
+++ d2_vectors.cpp
@@ -7,24 +7,24 @@ int main(){
 
     //Printing Vector
     
-    vector<int> v(10);
-    int s = v.size();
-    cout<<"Vector size- "<<s<<endl;
+    // vector<int> v(10);
+    // int s = v.size();
+    // cout<<"Vector size- "<<s<<endl;
     
-    cout<<"Vector - ";
-    for(int i=0;i<s;i++){
-        cout<<v[i]<<" ";
-    }
+    // cout<<"Vector - ";
+    // for(int i=0;i<s;i++){
+    //     cout<<v[i]<<" ";
+    // }
 
-    cout<<endl<<"Vector - ";
-    for(int i=0;i<s;i++){
-        cout<<v.at(i)<<" ";
-    }
+    // cout<<endl<<"Vector - ";
+    // for(int i=0;i<s;i++){
+    //     cout<<v.at(i)<<" ";
+    // }
 
-    cout<<endl<<"Vector - ";
-    for(int num : v){
-        cout<<num<<" ";
-    }
+    // cout<<endl<<"Vector - ";
+    // for(int num : v){
+    //     cout<<num<<" ";
+    // }
 
 
     return 0;
