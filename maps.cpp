a62ba43649d2620dc4fc50d2ed12b23a8399b42f
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-01-18 23:20:14 +0530

    ..

--- maps.cpp
+++ maps.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
-#include<vector>
+#include<map>
 
 using namespace std;
 
 int main(){
+    map<int, int> mp1;
+
+    mp1[1]=1;
+    mp1[2]=4;
+    mp1[3]=9;
+    mp1[4]=16;
+    mp1[5]=25;
+
+    for(int i=0; i<=mp1.size(); i++){
+        cout<<i<<" - ";
+        cout<<mp1[i]<<endl;
+    }
+
+    
+
 
     return 0;
 }
