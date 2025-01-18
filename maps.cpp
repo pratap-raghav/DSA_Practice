9051da9866350e0e2fedae7eaeb9f70a1e9a9913
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-01-18 23:28:38 +0530

    insert

--- maps.cpp
+++ maps.cpp
@@ -6,19 +6,26 @@ using namespace std;
 int main(){
     map<int, int> mp1;
 
-    mp1[1]=1;
-    mp1[2]=4;
-    mp1[3]=9;
-    mp1[4]=16;
-    mp1[5]=25;
+    // mp1[1]=1;
+    // mp1[2]=4;
+    // mp1[3]=9;
+    // mp1[4]=16;
+    // mp1[5]=25;
 
-    for(int i=0; i<=mp1.size(); i++){
-        cout<<i<<" - ";
-        cout<<mp1[i]<<endl;
+    // cout<<"Map - "<<endl;
+    // for(int i=1; i<=mp1.size(); i++){
+    //     cout<<i<<" - ";
+    //     cout<<mp1[i]<<endl;
+    // }
+
+    for(int i = 0; i<=10; i++){
+        mp1.insert({i, i*i*i});
     }
 
-    
-
+    cout<<endl<<"Map Cubes - "<<endl;
+    for(auto it : mp1){
+        cout<<it.first<<" - "<<it.second<<endl;
+    }
 
     return 0;
 }
