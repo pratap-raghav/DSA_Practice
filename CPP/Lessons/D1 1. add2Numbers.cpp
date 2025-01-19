81093c799dbb2a52b4a25f1574b1d48c639a4d10
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-01-19 19:14:30 +0530

    .

---
+++ CPP/Lessons/D1 1. add2Numbers.cpp
@@ -0,0 +1,13 @@
+//Add 2 numbers in C++
+
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b;
+	cout<<"Adding 2 numbers in C++\n\nNumber 1: ";
+	cin>>a;
+	cout<<"Number 2: ";
+	cin>>b;
+	cout<<"\nSum of "<<a<<" & "<<b<<": "<<a+b;
+	return 0;
+}
