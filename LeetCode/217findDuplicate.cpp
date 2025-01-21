bfc0b46d11d90d43a77ec75aac307ced4e6bb028
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-01-21 23:43:06 +0530

    Update 217findDuplicate.cpp

--- LeetCode/217findDuplicate.cpp
+++ LeetCode/217findDuplicate.cpp
@@ -22,32 +22,26 @@ using namespace std;
 //     }
 // };
 
-bool containsDuplicate(vector<int> & array) {
+bool containsDuplicate(vector<int> & nums) {
     map<int, int> m;
 
-    for (int i = 0; i < array.size(); i++)
+    for (int i = 0; i < nums.size(); i++)
     {
-        int key = array[i];
+        int key = nums[i];
         // loc = m.find(key);
-
         if(m.find(key) == m.end()){
             m.insert({key, 1});
         }
         else{
-            //cout<<"Duplicate Element"<<endl;
-
             return true;
         }
-
     }
     return false;
 }
 
-
 int main(){
 
     vector<int> arr = {1,2,3,4,5};
-
     cout<<"Vector contains duplicate(0-False / 1-True)- "<<containsDuplicate(arr);
 
     return 0;
