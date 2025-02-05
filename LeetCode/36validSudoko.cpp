c9409f2d215ed7f837486bf07910aed4892c0b68
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-02-06 01:42:44 +0530

    ...

--- LeetCode/36validSudoko.cpp
+++ LeetCode/36validSudoko.cpp
@@ -7,12 +7,17 @@ bool isValidSudoku(vector<vector<char>> &board)
     int s = board.size();
     for (int i = 0; i < s; i++)
     {
-        unordered_map<int, int> mHor, mVer;
+        unordered_map<char, int> mHor, mVer;
         for (int j = 0; j < s; j++)
         {
             char key1 = board[i][j];
             char key2 = board[j][i];
-            cout<<key1<<"\t"<<key2<<endl;
+            // cout<<key1<<"\t"<<key2<<endl;
+            cout<<i<<","<<j<<"\t";
+            if(j%3==2){
+                cout<<"\t";
+            }
+
             if (key1 != '.')
             {   
                 if (mHor.find(key1) == mHor.end())
@@ -36,7 +41,67 @@ bool isValidSudoku(vector<vector<char>> &board)
                 }
             }
         }
-        cout<<endl;
+        cout<<endl<<endl<<endl;
+        if(i%3==2){
+            cout<<endl<<endl<<endl;
+        }
+
+
+        int k = 0;
+        unordered_map<char, int> m1, m2, m3;
+        int s1=0, s2=3, s3=6;
+        while(k<s){
+            char key1 = board[k][s1];
+            char key2 = board[k][s2];
+            char key3 = board[k][s3];
+
+            if (key1 != '.')
+            { 
+                if (m1.find(key1) == m1.end())
+                {
+                    m1.insert({key1, 1});
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (key2 != '.')
+            { 
+                if (m2.find(key2) == m2.end())
+                {
+                    m2.insert({key2, 1});
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (key3 != '.')
+            { 
+                if (m3.find(key3) == m3.end())
+                {
+                    m3.insert({key3, 1});
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if(k%3==2){
+                m1.clear();
+                m1.clear();
+                m3.clear();
+                s1=0;
+                s2=3;
+                s3=6;
+            }
+
+            k++;
+        }
     }
 
     return true;
--- LeetCode/tempCodeRunnerFile.cpp
+++ LeetCode/tempCodeRunnerFile.cpp
@@ -1,33 +1,27 @@
-int s = board.size();
-    // for (int i = 0; i < s; i++)
-    // {
-    //     unordered_map<int, int> mHor, mVer;
-    //     for (int j = 0; j < s; j++)
-    //     {
-    //         char key1 = board[i][j];
-    //         char key2 = board[j][i];
+int k = 0;
+        // unordered_map<char, int> m1, m1, m3;
+        // int s1=0, s2=3, s3=6;
+        // while(k<s){
+        //     char key1 = board[k][s1];
+        //     char key2 = board[k][s2];
+        //     char key3 = board[k][s3];
 
-    //         if (key1 != '.')
-    //         {
-    //             if (mHor.find(key1) == mHor.end())
-    //             {
-    //                 mHor.insert({key1, 1});
-    //             }
-    //             else
-    //             {
-    //                 return false;
-    //             }
-    //         }
-    //         if (key2 != '.')
-    //         {
-    //             if (mVer.find(key2) == mVer.end())
-    //             {
-    //                 mVer.insert({key2, 1});
-    //             }
-    //             else
-    //             {
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    // }
+        //     if (key1 != '.')
+        //     { 
+        //         if (mVer.find(key2) == mVer.end())
+        //         {
+        //             mVer.insert({key2, 1});
+        //         }
+        //         else
+        //         {
+        //             return false;
+        //         }
+        //     }
+
+        //     if(k%3==2){
+        //         unordered_map<char, int> m1, m1, m3;
+        //         int s1=0, s2=3, s3=6;
+        //     }
+
+        //     k++;
+        // }
