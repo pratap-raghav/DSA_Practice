7bbb4f405df2ea38512c9638af8bd90552c3a0dd
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-02-07 23:31:59 +0530

    ...

--- LeetCode/150evalRPN.cpp
+++ LeetCode/150evalRPN.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 int evalRPN(vector<string>& tokens) {
         stack<int> s;
-        int val1, val2;
-        for(string it : tokens){
-            if(it == "+"){
+        int val1, val2, st = tokens.size();
+
+        for(int i=0; i<st; i++){
+            if(tokens[i] == "+"){
                 val2 = s.top();
                 s.pop();
                 val1 = s.top();
                 s.pop();
                 s.push(val1+val2);
             }
-            else if(it == "-"){
+            else if(tokens[i] == "-"){
                 val2 = s.top();
                 s.pop();
                 val1 = s.top();
                 s.pop();
                 s.push(val1-val2);
             }
-            else if(it == "*"){
+            else if(tokens[i] == "*"){
                 val2 = s.top();
                 s.pop();
                 val1 = s.top();
@@ -28,7 +29,7 @@ int evalRPN(vector<string>& tokens) {
                 s.push(val1*val2);
 
             }
-            else if(it == "/"){
+            else if(tokens[i] == "/"){
                 val2 = s.top();
                 s.pop();
                 val1 = s.top();
@@ -36,7 +37,7 @@ int evalRPN(vector<string>& tokens) {
                 s.push(val1/val2);
             }
             else{
-                s.push(stoi(it));
+                s.push(stoi(tokens[i]));
             }
         }
         return s.top();
