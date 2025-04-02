4ba3abbe8aebc6e04a1144ecb10e9438a9a716a2
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-02 21:07:05 +0530

    ...

---
+++ gfg/ropeCutR.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cutRope(int n, int a, int b, int c, int parts){
+    if(n==0){
+        return parts;
+    }
+    if(n<0){
+        return -1;
+    }
+    return max({
+        cutRope(n-a,a,b,c,parts++),
+        cutRope(n-b,a,b,c,parts++),
+        cutRope(n-c,a,b,c,parts++)
+    });
+}
+
+int main(){
+    cout<<cutRope(23,11,9,12,1);
+    return 0;
+}
---
+++ gfg/test.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<max({12,100,8});
+    return 0;
+}
