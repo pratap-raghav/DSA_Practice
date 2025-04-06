85de70535f996a35f55bc724e3456cd1871ea5c5
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-07 01:15:47 +0530

    .

--- gfg/Factorial.cpp
+++ gfg/Factorial.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int factorial(int N){
+    if(N==0 || N==1) return 1;
+    return(N*factorial(N-1));
+}
 
+int main(){
+    cout<<factorial(10);
     return 0;
 }
