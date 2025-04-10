582b875c03651c058e58e393e709c909a3897cae
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-11 02:04:36 +0530

    Update operations.cpp

--- gfg/arrays/operations.cpp
+++ gfg/arrays/operations.cpp
@@ -10,6 +10,30 @@ int search(int* arr, int n, int element){
     return -1;
 }
 
+int insert(int* arr, int n, int size, int element, int index){
+    if(n<size){
+        for(int i=n; i>-index; i--){
+            arr[i+1]=arr[i];
+        }
+        arr[index]=element;
+        cout<<"Element added"<<endl;
+        return n+1;
+    }
+    cout<<"Array already full, element not added"<<endl;
+}
+
+int deleteElement(int* arr, int element, int n){
+    int index = search(arr, n, element);
+    if(index!=-1){
+        for(int i=index; i<=n; i++){
+            arr[i]=arr[i+1];
+        }
+        return n-1;
+    }
+    return n;
+}
+
+
 int main(){
     int arr[] = {1,2,3,4,5,6,7,8,9,0};
     int index = search(arr, 10, 8);
