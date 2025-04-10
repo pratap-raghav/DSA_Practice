bc349eadc57d743274d467aa3dd2645af7e536de
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-11 01:42:03 +0530

    ...

--- gfg/arrays/operations.cpp
+++ gfg/arrays/operations.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int search(int* arr, int n, int element){
+    for(int i=0; i<n; i++){
+        if(arr[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
 
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,0};
+    int index = search(arr, 10, 8);
+    cout<<index;
     return 0;
 }
