dc8e2e6b2c25f89c724c619c3500f0f658db3a55
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-11 10:43:13 +0530

    update

---
+++ gfg/arrays/secGreatestElement.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int SecondMaxElement(vector <int> v){
+    int fmax = INT_MIN, smax = INT_MIN;
+
+    int size = v.size();
+    for (int i = 0; i < size; i++)
+    {
+        if(fmax < v.at(i)){
+            smax = fmax;
+            fmax = v.at(i);
+        }
+        if(smax<v.at(i) && v.at(i)<fmax){
+            smax = v.at(i);
+        }
+    }
+    return smax;
+}
+
+int main()
+{
+    vector<int> v = {12, 43, 65, 13, 100, -1, -43, 159, 200};
+    
+    int maximum = SecondMaxElement(v);
+    cout << maximum;
+    return 0;
+}
