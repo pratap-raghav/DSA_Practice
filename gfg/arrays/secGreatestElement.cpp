07650eae14fa2186dea01ddb7564486bc070d3d4
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-13 01:12:43 +0530

    update

--- gfg/arrays/secGreatestElement.cpp
+++ gfg/arrays/secGreatestElement.cpp
@@ -1,37 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int SecondMaxElement(vector <int> v){
-    int fmax = INT_MIN, smax = INT_MIN;
-
+int maxElement(vector<int> &v)
+{
+    int maximum = INT_MIN;
     int size = v.size();
-
-    if(size<2)
-        return -1;
-
-    // int res = -1;
-
     for (int i = 0; i < size; i++)
     {
-        if(fmax < v.at(i)){
-            smax = fmax;
-            // res = i;
-            fmax = v.at(i);
-        }
-        if(smax<v.at(i) && v.at(i)<fmax){
-            smax = v.at(i);
-            // res = 1;
+        maximum = maximum < v.at(i) ? v.at(i) : maximum;
+    }
+    return maximum;
+}
+
+int SecondMaxElement(vector<int> &v)
+{
+    int size = v.size();
+    int smax = -1;
+    if (size < 2)
+        return smax;
+
+    int fmax = maxElement(v);
+    for (int i = 0; i < size; i++)
+    {
+        if (v.at(i) < fmax)
+        {
+            if (smax == -1)
+                smax = i;
+            if (v.at(smax) < v.at(i))
+            {
+                smax = i;
+            }
         }
     }
-    // return res;
     return smax;
+    // int fmax = INT_MIN, smax = INT_MIN;
+    // // int res = -1;
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     if(fmax < v.at(i)){
+    //         smax = fmax;
+    //         // res = i;
+    //         fmax = v.at(i);
+    //     }
+    //     if(smax<v.at(i) && v.at(i)<fmax){
+    //         smax = v.at(i);
+    //         // res = 1;
+    //     }
+    // }
+    // // return res;
+    // return smax;
+
+    // int fmax = 0, smax = 0;
+    // for(int i = 0; i<size; i++){
+    //     cout<<v.at(i)<<" ";
+    //     if(v.at(fmax)<v.at(i)){
+    //         smax = fmax;
+    //         fmax = i;
+    //     }
+    //     if(v.at(smax) < v.at(i) && v.at(i)<v.at(fmax)){
+    //         smax = i;
+    //     }
+    // }
+    // return smax;
 }
 
 int main()
-{                     
-    //                0   1   2   3   4    5    6    7    8
-    vector<int> v = {12, 43, 65, 13, 100, -1, -43, 159, 200};
-    
+{
+    //                0   1   2   3   4    5    6    7    8   9
+    vector<int> v = {1222, 43, 65, 13, 100, -1, -43, 159, 200, 199};
+
     int maximum = SecondMaxElement(v);
     cout << maximum;
     return 0;
