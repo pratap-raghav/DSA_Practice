df7d42b2f5c57993e3be413f01751d369baddf6e
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-13 18:18:54 +0530

    update

--- gfg/arrays/move0ToEnd.cpp
+++ gfg/arrays/move0ToEnd.cpp
@@ -12,42 +12,59 @@ void printVector(vector<int> &v)
     cout << endl;
 }
 
-
-
-// // Approach 2 Time: O(n), Aux: Th(1)
+// // Approach 1 Time: O(n * (no.of 0s)), Aux: Th(1)
 // void move0ToEnd(vector<int> &v)
 // {
 //     int size = v.size(), loc = 0;
 
 //     for (int i = 0; i < size; i++)
 //     {
-//         if (v[i] != 0)
-//         {
-//             v[loc] = v[i];
-//             loc++;
+//         if(v[i] == 0){
+//             for(int j=i; j<size; j++){
+//                 v[j]=v[j+1];
+//             }
+//             v[size-1]=0;
 //         }
-//     }
-//     for (int i = loc; i < size; i++)
-//     {
-//         v[i] = 0;
 //     }
 // }
 
-// Approach 3(from GFG) Time: O(n), Aux: Th(1)
-void move0ToEnd(vector<int> &v){
+
+// Approach 2 Time: O(n), Aux: Th(1)
+void move0ToEnd(vector<int> &v)
+{
     int size = v.size(), loc = 0;
 
     for (int i = 0; i < size; i++)
     {
         if (v[i] != 0)
-        {   
-            int temp = v[loc];
+        {
             v[loc] = v[i];
-            v[i] = temp;
             loc++;
         }
     }
+    for (int i = loc; i < size; i++)
+    {
+        v[i] = 0;
+    }
 }
+
+
+
+// // Approach 3 (from GFG) Time: O(n), Aux: Th(1)
+// void move0ToEnd(vector<int> &v){
+//     int size = v.size(), loc = 0;
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (v[i] != 0)
+//         {   
+//             int temp = v[loc];
+//             v[loc] = v[i];
+//             v[i] = temp;
+//             loc++;
+//         }
+//     }
+// }
 
 int main()
 {
