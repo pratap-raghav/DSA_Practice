beb240f68c72957b52f55d563132945d7ecfa455
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-18 01:57:14 +0530

    update

--- gfg/arrays/equilibriumIdx.cpp
+++ gfg/arrays/equilibriumIdx.cpp
@@ -1,23 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// //Naive approach Time Complexity : O(n^2) Auxiliary Space : O(1)
+// int equilibriumInx(vector<int> v)
+// {
+//     int size = v.size();
+//     for (int i = 0; i < size; i++)
+//     {
+//         int l = 0, r = 0;
+//         for (int j = 0; j < i; j++)
+//         {
+//             l = l + v[j];
+//         }
+//         for (int j = i + 1; j < size; j++)
+//         {
+//             r = r + v[j];
+//         }
+//         cout << i << "(" << v[i] << "):\t" << l << "\t" << r << endl;
+//         if (l == r)
+//             return i;
+//     }
+//     return -1;
+// }
+
+//Approach 2 Time Complexity : O(n) Auxiliary Space : O(1)
 int equilibriumInx(vector<int> v)
 {
-    int size = v.size();
+    int size = v.size(), sum = 0, l = 0;
     for (int i = 0; i < size; i++)
     {
-        int l = 0, r = 0;
-        for (int j = 0; j < i; j++)
+        sum += v[i];
+    }
+    for (int i = 0; i < size; i++)
+    {
+        sum -= v[i];
+        if (l == sum)
         {
-            l = l + v[j];
-        }
-        for (int j = i + 1; j < size; j++)
-        {
-            r = r + v[j];
-        }
-        cout << i << "(" << v[i] << "):\t" << l << "\t" << r << endl;
-        if (l == r)
             return i;
+        }
+        l += v[i];
     }
     return -1;
 }
