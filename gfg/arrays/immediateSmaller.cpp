672ef4c6c51c4b4d6b7e96899f52098f2116e846
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-18 03:37:25 +0530

    update

---
+++ gfg/arrays/immediateSmaller.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int immediateSmaller(int arr[], int n, int x)
+    {
+        // your code here
+        int element=-1, diff=INT_MAX;
+        
+        for(int i = 0; i < n; i++){
+        
+            if(arr[i]<x){
+                if ((x-arr[i])<diff){
+                    diff = x-arr[i];
+                    element = arr[i];
+                }
+            }
+        }
+        return element;        
+    }
+
+int main(){
+
+    return 0;
+}
--- gfg/arrays/isSorted.cpp
+++ gfg/arrays/isSorted.cpp
@@ -1,11 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isSorted(vector<int> v){
+bool isSorted(vector<int> v)
+{
     int size = v.size();
     int cMax = v.at(0);
-    for(int i=1; i<size;i++){
-        if(cMax>v.at(i)){
+    for (int i = 1; i < size; i++)
+    {
+        if (cMax > v.at(i))
+        {
             return false;
         }
         cMax = v.at(i);
@@ -13,8 +16,36 @@ bool isSorted(vector<int> v){
     return true;
 }
 
-int main(){
-    vector<int> v = {1,2,3,4,5,6,7,8,9};
-    cout<<isSorted(v);
+int isSorted(int arr[], int n)
+{
+    int flagA = 1, flagD = 1, curr = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+
+        if (curr > arr[i])
+        {
+            flagA = 0;
+        }
+        curr = arr[i];
+    }
+    curr = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+
+        if (curr < arr[i])
+        {
+            flagD = 0;
+        }
+        curr = arr[i];
+    }
+    return (flagA || flagD);
+}
+
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout << isSorted(v);
     return 0;
 }
