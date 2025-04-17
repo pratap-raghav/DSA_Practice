d73e3da9b33968e0a0eb36e8e53ea8243d9307bc
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-18 01:48:05 +0530

    update

---
+++ gfg/arrays/equilibriumIdx.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int equilibriumInx(vector<int> v)
+{
+    int size = v.size();
+    for (int i = 0; i < size; i++)
+    {
+        int l = 0, r = 0;
+        for (int j = 0; j < i; j++)
+        {
+            l = l + v[j];
+        }
+        for (int j = i + 1; j < size; j++)
+        {
+            r = r + v[j];
+        }
+        cout << i << "(" << v[i] << "):\t" << l << "\t" << r << endl;
+        if (l == r)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> v = {-7, 1, 5, 2, -4, 3, 0};
+    cout << equilibriumInx(v) << endl;
+    return 0;
+}
