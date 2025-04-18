06aea4c7a37ba3ea29e0dfc87ec1461f7f978383
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-18 17:01:50 +0530

    Update tSearchRecursion.cpp

--- gfg/search/tSearchRecursion.cpp
+++ gfg/search/tSearchRecursion.cpp
@@ -1,31 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int tSearch(vector<int> &v, int &element, int start, int end){
-    if(start<=end){
+int tSearch(vector<int> &v, int &element, int start, int end)
+{
+    if (start <= end)
+    {
         int mid1 = start + (end - start) / 3;
         int mid2 = end - (end - start) / 3;
-        
-        if (v[mid1] == element){
+
+        if (v[mid1] == element)
+        {
             return mid1;
         }
 
-        if (v[mid2] == element){
+        if (v[mid2] == element)
+        {
             return mid2;
         }
 
-        if (element < v[mid1]){
+        if (element < v[mid1])
+        {
             // end = mid1 - 1;
             return tSearch(v, element, start, mid1 - 1);
-            
         }
 
-        else if (v[mid2] < element){
+        else if (v[mid2] < element)
+        {
             // start = mid2 + 1;
             return tSearch(v, element, mid2 + 1, end);
         }
 
-        else{
+        else
+        {
             // start = mid1 + 1;
             // end = mid2 - 1;
             return tSearch(v, element, mid1 + 1, mid2 - 1);
@@ -36,9 +42,10 @@ int tSearch(vector<int> &v, int &element, int start, int end){
     return -1;
 }
 
-int main(){
+int main()
+{
     vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int element = 6;
-    cout << tSearch(v, element, 0, v.size()-1);
+    cout << tSearch(v, element, 0, v.size() - 1);
     return 0;
 }
