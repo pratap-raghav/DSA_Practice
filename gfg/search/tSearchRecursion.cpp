97eea5ef181353000469fa5a2185ab61a9584a4e
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-04-18 17:01:39 +0530

    update

--- gfg/search/tSearchRecursion.cpp
+++ gfg/search/tSearchRecursion.cpp
@@ -15,19 +15,22 @@ int tSearch(vector<int> &v, int &element, int start, int end){
         }
 
         if (element < v[mid1]){
-            end = mid1 - 1;
+            // end = mid1 - 1;
+            return tSearch(v, element, start, mid1 - 1);
             
         }
 
         else if (v[mid2] < element){
-            start = mid2 + 1;
+            // start = mid2 + 1;
+            return tSearch(v, element, mid2 + 1, end);
         }
 
         else{
-            start = mid1 + 1;
-            end = mid2 - 1;
+            // start = mid1 + 1;
+            // end = mid2 - 1;
+            return tSearch(v, element, mid1 + 1, mid2 - 1);
         }
-        return tSearch(v, element, start, end);
+        // return tSearch(v, element, start, end);
     }
 
     return -1;
@@ -35,7 +38,7 @@ int tSearch(vector<int> &v, int &element, int start, int end){
 
 int main(){
     vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int element = 10;
+    int element = 6;
     cout << tSearch(v, element, 0, v.size()-1);
     return 0;
 }
