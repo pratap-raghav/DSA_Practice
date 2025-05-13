72c0a028d26bac3b71c7feb63af94bfb5e6763f4
pratap-raghav <pratap.raghav.dev@gmail.com>
2025-05-14 04:13:33 +0530

    ...

--- gfg/sorting/selection.cpp
+++ gfg/sorting/selection.cpp
@@ -12,29 +12,51 @@ void printVector(vector<int> &v)
     cout << endl;
 }
 
+// // Naive implementation
+// void selectionSort(vector<int> &v)
+// {
+//     int size = v.size();
+//     vector<int> temp;
+
+//     for (int i = 0; i < size; i++)
+//     {
+
+//         int min = 0;
+//         for (int j = 1; j < size; j++)
+//         {
+//             if (v[min] > v[j])
+//             {
+//                 min = j;
+//             }
+//         }
+//         temp.push_back(v[min]);
+//         v[min] = INT_MAX;
+//     }
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         v[i] = temp[i];
+//     }
+// }
+
 void selectionSort(vector<int> &v)
 {
     int size = v.size();
-    vector<int> temp;
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size - 1; i++)
     {
 
-        int min = 0;
-        for (int j = 1; j < size; j++)
+        int min = i;
+        for (int j = i + 1; j < size; j++)
         {
             if (v[min] > v[j])
             {
                 min = j;
             }
         }
-        temp.push_back(v[min]);
-        v[min] = INT_MAX;
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        v[i] = temp[i];
+        int temp = v[i];
+        v[i] = v[min];
+        v[min] = temp;
     }
 }
 
